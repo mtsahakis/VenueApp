93b127d8a817e324c79ddc241fda367332fa0755
mtsahakis <manos.tsahakis@fluidic.com>
2019-10-28 17:14:41 +0000

    Adding UI widgets, search basic functionality and timber library.

--- app/src/main/java/com/mtsahakis/venues/VenueActivity.kt
+++ app/src/main/java/com/mtsahakis/venues/VenueActivity.kt
@@ -1,13 +1,60 @@
 package com.mtsahakis.venues
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_venue.*
+import android.view.Menu
+import android.view.inputmethod.EditorInfo
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+
 
 class VenueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_venue)
+
+        if (savedInstanceState == null) {
+            setUpView()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            imeOptions = EditorInfo.IME_ACTION_SEARCH
+            queryHint = getString(R.string.search_hint)
+            isFocusable = true
+            isIconified = false
+            requestFocusFromTouch()
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    onSearchStart(query)
+                    return false
+                }
+
+                override fun onQueryTextChange(query: String): Boolean {
+                    //get all text changes
+                    return false
+                }
+            })
+        }
+        return true
+    }
+
+    private fun onSearchStart(query: String) {
+        Timber.e("query: $query")
+        hideHint()
+    }
+
+    private fun setUpView() {
+
+    }
+
+    private fun hideHint() {
+
     }
 }
---
+++ app/src/main/java/com/mtsahakis/venues/VenueApp.kt
@@ -0,0 +1,38 @@
+package com.mtsahakis.venues
+
+import android.app.Application
+import android.util.Log
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class VenueApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // set up timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(object : Timber.Tree() {
+
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+                        return
+                    }
+
+                    when (priority) {
+                        Log.WARN -> Log.w(tag, message)
+                        Log.ERROR -> if (t == null) {
+                            Log.e(tag, message)
+                        } else {
+                            Log.e(tag, message, t)
+                        }
+                        else -> Log.wtf(tag, message)
+                    }
+                }
+            })
+        }
+    }
+}
