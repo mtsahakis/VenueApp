d5efc1337b391aed4a57b8c789fa58bc15c0fcfe
mtsahakis <manos.tsahakis@fluidic.com>
2019-10-28 11:45:02 +0000

    project setup

---
+++ app/src/main/java/com/mtsahakis/venues/VenueActivity.kt
@@ -0,0 +1,13 @@
+package com.mtsahakis.venues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_venue.*
+
+class VenueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venue)
+    }
+}
